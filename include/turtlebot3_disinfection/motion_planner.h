61094d0b671b55ad0c8d04e23f5f11757eee3877
David Young <d.young0851@gmail.com>
2020-11-10 04:13:54 +1100

    Started wall-following algorithm in motion_planner

--- include/turtlebot3_disinfection/motion_planner.h
+++ include/turtlebot3_disinfection/motion_planner.h
@@ -4,49 +4,31 @@
 #include "ros/ros.h"
 
 #include <iostream>
+#include <algorithm>
 #include <string>
 #include <vector>
 #include <sensor_msgs/LaserScan.h>
 #include <geometry_msgs/Twist.h>
-#include <geometry_msgs/TransformStamped.h>
-#include <geometry_msgs/PoseStamped.h>
-#include <nav_msgs/OccupancyGrid.h>
-#include <nav_msgs/MapMetaData.h>
-#include <tf2_ros/transform_listener.h>
 
-#define POSITION_THRESHOLD 0.05f
 
 class MotionPlanner
 {
     public:
         MotionPlanner(ros::NodeHandle* nh);
 
-        void publishWaypoint(const std::vector<float> position, const std::vector<float> orientation);
+        void publishVelocity(const float lin_vel, const float ang_vel);
 
-        void updateWaypoint();
+        float getRange(const sensor_msgs::LaserScan& msg, int index);
 
-        // void publishVelocity(const std::vector<float> lin_vel, const std::vector<float> ang_vel);
-
-        // void laserCallback(const sensor_msgs::LaserScan& msg);
-
-        void mapCallback(const nav_msgs::OccupancyGrid& msg);
-
-        void mapPoseCallback(const geometry_msgs::PoseStamped& msg);
+        void laserCallback(const sensor_msgs::LaserScan& msg);
 
     private:
-        geometry_msgs::PoseStamped current_pose_;
 
-        geometry_msgs::PoseStamped waypoint_;
+        int mode_;
 
-        std::vector<std::vector<int>> waypoints_;
+        const ros::Publisher pub_vel_;
 
-        nav_msgs::OccupancyGrid map_;
-
-        ros::Publisher pub_waypoint_;
-
-        ros::Subscriber sub_map_;
-
-        ros::Subscriber sub_map_pose_;
+        const ros::Subscriber sub_laser_;
 };
 
 #endif
--- src/motion_planner.cpp
+++ src/motion_planner.cpp
@@ -4,62 +4,84 @@
 
 #include "turtlebot3_disinfection/motion_planner.h"
 
+#define WALL_DIST 0.2f
+#define MAX_LIN_VEL 0.22f
+#define MAX_ANG_VEL 2.84f
+#define P_LINEAR 1.0f
+#define P_ANGULAR 1.0f
+#define D_LINEAR 1.0f
+#define D_ANGULAR 1.0f
+
+#define FRONT 0
+#define LEFT 90
+#define LEFT_FRONT 85
+#define LEFT_BACK 95
+
+
+// const int SCAN_RANGES = {0, }
 
 MotionPlanner::MotionPlanner(ros::NodeHandle* nh)
+    : mode_(0)
+    , pub_vel_(nh->advertise<geometry_msgs::Twist>("/cmd_vel", 10))
+    , sub_laser_(nh->subscribe("/scan", 1000, &MotionPlanner::laserCallback, this))
 {
-    pub_waypoint_ = nh->advertise<geometry_msgs::PoseStamped>("/move_base_simple/goal", 10);
     // pub_vel_ = nh->advertise<geometry_msgs::Twist>("/cmd_vel", 10);
     // sub_laser_ = nh->subscribe("/scan", 1000, &MotionPlanner::laserCallback, this);
-    sub_map_ = nh->subscribe("/map", 10, &MotionPlanner::mapCallback, this);
-
-    sub_map_pose_ = nh->subscribe("/map_pose", 10, &MotionPlanner::mapPoseCallback, this);
 }
 
-
-void MotionPlanner::updateWaypoint()
+void MotionPlanner::publishVelocity(const float lin_vel, const float ang_vel)
 {
-    // Check if waypoint has been reached
+    geometry_msgs::Twist vel_msg;
 
-    // Compare the current pose with the destination pose
-    // TODO: Should change this to check Euclidean distance
-    if (abs(current_pose_.pose.position.x - waypoint_.pose.position.x) < POSITION_THRESHOLD &&
-        abs(current_pose_.pose.position.y - waypoint_.pose.position.y) < POSITION_THRESHOLD)
-    {
-        // waypoint_reached_ = true;
-        waypoint_.pose.position.x += 1;
-    }
+    vel_msg.linear.x = lin_vel;
+    vel_msg.linear.y = 0.0f;
+    vel_msg.linear.z = 0.0f;
+    vel_msg.angular.x = 0.0f;
+    vel_msg.angular.y = 0.0f;
+    vel_msg.angular.z = ang_vel;
+
+    pub_vel_.publish(vel_msg);
 }
 
-
-void MotionPlanner::publishWaypoint(const std::vector<float> position, const std::vector<float> orientation)
+float getRange(const sensor_msgs::LaserScan& msg, int index)
 {
-    geometry_msgs::PoseStamped msg;
-
-    msg.header.frame_id = "map";
-
-    msg.pose.position.x = position[0];
-    msg.pose.position.y = position[1];
-    msg.pose.position.z = position[2];
-
-    msg.pose.orientation.x = orientation[0];
-    msg.pose.orientation.y = orientation[1];
-    msg.pose.orientation.z = orientation[2];
-    msg.pose.orientation.w = orientation[3];
-
-    pub_waypoint_.publish(msg);
+    return std::min(msg.ranges[index], msg.range_max);
 }
 
-
-void MotionPlanner::mapCallback(const nav_msgs::OccupancyGrid& msg)
+void MotionPlanner::laserCallback(const sensor_msgs::LaserScan& msg)
 {
-    // int8** map = msg.data;
-    map_ = msg;
-}
+    // positive -> turn left
+    float p_error = (msg.ranges[LEFT] - WALL_DIST) / msg.ranges[LEFT];
 
+    // positive -> turn left
+    float d_error = (msg.ranges[LEFT_FRONT] - msg.ranges[LEFT_BACK]) / (msg.ranges[LEFT_FRONT] + msg.ranges[LEFT_BACK]);
 
-void MotionPlanner::mapPoseCallback(const geometry_msgs::PoseStamped& msg)
-{
-    current_pose_ = msg;
+    float lin_vel = std::max(D_LINEAR / d_error, MAX_LIN_VEL);
+
+    float ang_vel = std::max(P_ANGULAR * p_error + D_ANGULAR * d_error, MAX_ANG_VEL);
+
+    publishVelocity(lin_vel, ang_vel);
+
+    // float front_range = msg.ranges[0];
+    // float left_range = msg.ranges[90];
+    //
+    // if (front_range > WALL_DIST && left_range > WALL_DIST)
+    // {
+    //     publishVelocity(0.6 * MAX_LIN_VEL, -0.3 * MAX_ANG_VEL);
+    // }
+    // else if (front_range < WALL_DIST && left_range > WALL_DIST)
+    // {
+    //     publishVelocity(0.0f, 0.2 * MAX_ANG_VEL);
+    // }
+    // else if (front_range > WALL_DIST && left_range < WALL_DIST)
+    // {
+    //     publishVelocity(0.6 * MAX_LIN_VEL, 0.0f);
+    // }
+    // else
+    // {
+    //     publishVelocity(0.0f, 0.6 * MAX_ANG_VEL);
+    // }
+
 }
 
 
@@ -71,23 +93,28 @@ int main(int argc, char **argv)
 
     MotionPlanner motion_planner = MotionPlanner(&nh);
 
-    ros::Rate loop_rate(10);
+    // ros::Rate loop_rate(10);
 
-    while (ros::ok())
-    {
-        ros::spinOnce();
+    // initialise 0-vectors
+    // std::vector<float> position(3, 0);
+    // std::vector<float> orientation(4, 0);
+    // position[0] = 1;
+    // orientation[3] = 1;
+    //
+    // motion_planner.publishWaypoint(position, orientation);
 
-        // initialise 0-vectors
-        std::vector<float> position(3, 0);
-        std::vector<float> orientation(4, 0);
+    ros::spin();
 
-        position[0] = 2;
-        orientation[3] = 1;
-
-        motion_planner.publishWaypoint(position, orientation);
-
-        loop_rate.sleep();
-    }
+    // while (ros::ok())
+    // {
+    //     ros::spinOnce();
+    //
+    //
+    //     // motion_planner.updateWaypoint();
+    //
+    //
+    //     loop_rate.sleep();
+    // }
 
     return 0;
 }
